f28f8aa024fb08b451c6652ba7db7547268ef9b2
Firzan Armani <mrfirzanarmani@gmail.com>
2023-04-02 22:46:53 +0800

    feat(client): add telegram login button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,20 @@
-import "./App.css";
+import { LoginButton } from "@telegram-auth/react";
 
 function App() {
-  return <div className="h-screen bg-black"></div>;
+  return (
+    <div className="h-screen bg-white">
+      <LoginButton
+        botUsername="shoplisterapp_bot"
+        onAuthCallback={(data) => {
+          console.log(data);
+        }}
+        buttonSize="large"
+        cornerRadius={10}
+        requestAccess={"write"}
+        showAvatar
+      />
+    </div>
+  );
 }
 
 export default App;
