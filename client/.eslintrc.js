4c211c4be1be1ddd5b257c1b9dd37c8343112f92
Firzan Armani <mrfirzanarmani@gmail.com>
2023-04-04 10:28:58 +0800

    chore(client): clean up configs and add preline

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,21 +1,25 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "standard-with-typescript",
+    "prettier",
   ],
-  overrides: [
-  ],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
   },
-  plugins: [
-    'react'
-  ],
-  rules: {
-  }
-}
+  plugins: ["react"],
+  rules: {},
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import { LoginButton } from "@telegram-auth/react";
+import { type ReactElement } from "react";
 
-function App() {
+function App(): ReactElement {
   return (
     <div className="h-screen bg-white">
       <LoginButton
         botUsername="shoplisterapp_bot"
-        authCallbackUrl="/api/v1/login/"
+        onAuthCallback={(data) => {
+          console.log(data);
+        }}
         buttonSize="large"
         cornerRadius={10}
         requestAccess={"write"}
