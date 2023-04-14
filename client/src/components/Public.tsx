ef06779d04b1de7af1b16656f3e64e24be2508b0
Firzan Armani <mrfirzanarmani@gmail.com>
2023-04-14 18:36:50 +0800

    fix(client): fix buttons gap on main page

--- client/src/components/Public.tsx
+++ client/src/components/Public.tsx
@@ -20,7 +20,7 @@ function Public(): ReactElement {
             Go to dashboard
           </button>
         ) : (
-          <div className="gap-x-2">
+          <div className="flex gap-x-4">
             <button
               type="button"
               className="inline-flex items-center justify-center gap-2 rounded-md border border-transparent bg-blue-500 px-4 py-3 text-sm font-semibold text-white transition-all hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800"
