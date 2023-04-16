9d22aaa74170a99438f2dfd9e23eca4f10dd6281
Firzan Armani <mrfirzanarmani@gmail.com>
2023-04-16 18:45:30 +0800

    fix(server): undo mutex implementation

--- client/src/app/api/apiSlice.ts
+++ client/src/app/api/apiSlice.ts
@@ -7,9 +7,6 @@ import {
 } from "@reduxjs/toolkit/query/react";
 import { type RootState } from "../store";
 import { logout, setCredentials } from "../../features/auth/authSlice";
-import { Mutex } from "async-mutex";
-
-const mutex = new Mutex();
 
 // https://redux-toolkit.js.org/rtk-query/usage/examples#dispatching-an-action-to-set-the-user-state
 // https://redux-toolkit.js.org/rtk-query/api/fetchBaseQuery#setting-default-headers-on-requests
@@ -34,53 +31,36 @@ const baseQueryWithReauth: BaseQueryFn<
   unknown,
   FetchBaseQueryError
 > = async (args, api, extraOptions) => {
-  await mutex.waitForUnlock();
-
   // Try the initial query
   let result = await baseQuery(args, api, extraOptions);
 
   // 401 Unauthorized - Access token has expired (verifyJWT fails)
   if (result.error !== undefined && result.error.status === 401) {
-    if (mutex.isLocked() === false) {
-      const release = await mutex.acquire();
+    // Query /refresh to try and get a new access token
+    const refreshResult = await baseQuery("/auth/refresh", api, extraOptions);
 
-      try {
-        // Query /refresh to try and get a new access token
-        const refreshResult = await baseQuery(
-          "/auth/refresh",
-          api,
-          extraOptions
-        );
+    const refreshResultData = refreshResult.data as
+      | { accessToken: string }
+      | undefined;
 
-        const refreshResultData = refreshResult.data as
-          | { accessToken: string }
-          | undefined;
+    if (refreshResultData !== undefined) {
+      // Store the new access token
+      api.dispatch(setCredentials({ ...refreshResultData }));
 
-        if (refreshResultData !== undefined) {
-          // Store the new access token
-          api.dispatch(setCredentials({ ...refreshResultData }));
-
-          // Then, retry the initial query
-          result = await baseQuery(args, api, extraOptions);
-        } else {
-          // Both access and refresh cookies have expired
-          if (
-            refreshResult.error !== undefined &&
-            refreshResult.error.status === 401
-          ) {
-            refreshResult.error.data = { message: "Login has expired" };
-          }
-
-          api.dispatch(logout());
-
-          return refreshResult;
-        }
-      } finally {
-        release();
+      // Then, retry the initial query
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      // Both access and refresh cookies have expired
+      if (
+        refreshResult.error !== undefined &&
+        refreshResult.error.status === 401
+      ) {
+        refreshResult.error.data = { message: "Login has expired" };
       }
 
-      await mutex.waitForUnlock();
-      result = await baseQuery(args, api, extraOptions);
+      api.dispatch(logout());
+
+      return refreshResult;
     }
   }
 
